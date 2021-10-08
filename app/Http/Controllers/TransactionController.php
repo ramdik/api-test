6013b7035b703547489552ef09cc05f05fc7e116
ramdik <ramdik@github.com>
2021-10-08 14:03:07 +0700

    api test

---
+++ app/Http/Controllers/TransactionController.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Transaction;
+use Illuminate\Database\QueryException;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Symfony\Component\HttpFoundation\Response;
+
+class TransactionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $transaction = Transaction::orderBy('time', 'DESC')->get();
+        $response = [
+            'message' => 'List transaction order by newer time',
+            'success' => true,
+            'data' => $transaction
+        ];
+
+        return response()->json($response, Response::HTTP_OK);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //adding rule with validator
+       $validator = Validator::make($request->all(), [
+           'title' => 'required',
+           'amount' => 'required|numeric',
+           'type' => 'required|in:expense,revenue'
+       ]);
+
+       // if validator req fails return 422 & err msg
+       if ($validator->fails()) {
+           return response()->json(
+               $validator->errors(),
+               Response::HTTP_UNPROCESSABLE_ENTITY
+           );
+       }
+
+       // if validator true do this
+       try {
+           $transaction = Transaction::create($request->all());
+           $response = [
+               'message' => 'Transaction success created',
+               'success' => true,
+               'data' => $transaction
+           ];
+
+           return response()->json($response, Response::HTTP_CREATED);
+       } catch (QueryException $exept) {
+           return response()->json([
+               'message' => 'Transaction fail ' . $exept->errorInfo
+           ]);
+       }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Transaction  $transaction
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $transaction = Transaction::findOrFail($id);
+        return response()->json([
+            'message' => 'Pencarian selesai',
+            'success' => true,
+            'data' => $transaction
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Transaction  $transaction
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Transaction $transaction)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Transaction  $transaction
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $transaction = Transaction::findOrFail($id);
+
+        //adding rule with validator
+        $validator = Validator::make($request->all(), [
+        'title' => 'required',
+        'amount' => 'required|numeric',
+        'type' => 'required|in:expense,revenue'
+    ]);
+
+    // if validator req fails return 422 & err msg
+    if ($validator->fails()) {
+        return response()->json(
+            $validator->errors(),
+            Response::HTTP_UNPROCESSABLE_ENTITY
+        );
+    }
+
+    // if validator true do this
+    try {
+        $transaction->update($request->all());
+        $response = [
+            'message' => 'Transaction success update',
+            'success' => true,
+            'data' => $transaction
+        ];
+
+        return response()->json($response, Response::HTTP_OK);
+    } catch (QueryException $exept) {
+        return response()->json([
+            'message' => 'Transaction fail ' . $exept->errorInfo
+        ]);
+    }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Transaction  $transaction
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Transaction $transaction)
+    {
+        //
+    }
+}
